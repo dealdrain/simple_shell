2067ac480198bb3abc382f925d42073d20c2cb00
Godfred <goddysmart2468@gmail.com>
2023-09-19 16:21:27 -0700

    add a new function prototype

--- shell.h
+++ shell.h
@@ -24,6 +24,7 @@ int exec_cmd(char **args, char **argv);
 int check_delim(char c, char *delim);
 char **custom_strtok(char *str, char *delim);
 char **custom_strtok2(char *str, char delim);
+void check_comment(char *buf);
 
 #endif /*OUR_SIMPLE_SHELL*/
 
